0b7a0e1f0e6ad0544d5d4ae8d4afa8fb5d1a0aa1
OBz <alongkorn.iem-o-pas@ktb.co.th>
2020-03-18 11:30:16 +0700

    updated, iteration

---
+++ iteration/iteration_test.go
@@ -0,0 +1,19 @@
+package iteration_test
+
+import "testing"
+import "github.com/devaof/learn-go-with-tests/iteration"
+
+func TestRepeat(t *testing.T) {
+	repeated := iteration.Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		iteration.Repeat("a")
+	}
+}
